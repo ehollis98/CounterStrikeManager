50bf3d5c7c173c15ee303b0bda5cf59505b1ba90
Eric Hollis <ehollis@madisoncollege.edu>
2018-12-18 03:40:58 -0600

    Java classes created for users and reports, users may have many reports.

---
+++ src/main/java/counterStrikeManager/entity/Report.java
@@ -0,0 +1,199 @@
+package counterStrikeManager.entity;
+
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+import java.util.Date;
+
+/**
+ *
+ * @author ehollis98
+ */
+@Entity(name = "Report")
+@Table(name ="report")
+public class Report {
+
+    /**
+     * Instantiates a new Report.
+     */
+    public Report() {
+    }
+
+    /**
+     * Instantiates a new Report.
+     *
+     * @param id                   the id
+     * @param name                 the report name
+     * @param date                 the date
+     * @param playerOneSteamNumber the steam number of player one
+     * @param playerTwoSteamNumber the steam number of player two
+     * @param statisticType        the statistic type
+     */
+    public Report(int id, String name, Date date, int playerOneSteamNumber, int playerTwoSteamNumber, String statisticType) {
+        this.id = id;
+        this.name = name;
+        this.date = date;
+        this.playerOneSteamNumber = playerOneSteamNumber;
+        this.playerTwoSteamNumber = playerTwoSteamNumber;
+        this.statisticType = statisticType;
+    }
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.AUTO, generator = "native")
+    @GenericGenerator(name = "native",strategy = "native")
+    private int id;
+
+    @Column(name = "date")
+    private Date date;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "steam_number_one")
+    private int playerOneSteamNumber;
+
+    @Column(name = "steam_number_two")
+    private int playerTwoSteamNumber;
+
+    @Column(name = "statistic_type")
+    private String statisticType;
+
+    @ManyToOne
+    @JoinColumn(name = "user_Id", foreignKey = @ForeignKey(name = "user_Id"))
+
+
+    private User user;
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+
+    /**
+     * Gets id.
+     *
+     * @return the id
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * Sets id.
+     *
+     * @param id the id
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    /**
+     * Gets date.
+     *
+     * @return the date
+     */
+    public Date getDate() {
+        return date;
+    }
+
+    /**
+     * Sets date.
+     *
+     * @param date the date
+     */
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+
+    /**
+     * Gets name.
+     *
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * Sets name.
+     *
+     * @param name the name
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * Gets player one steam number.
+     *
+     * @return the player one steam number
+     */
+    public int getPlayerOneSteamNumber() {
+        return playerOneSteamNumber;
+    }
+
+    /**
+     * Sets player one steam number.
+     *
+     * @param playerOneSteamNumber the player one steam number
+     */
+    public void setPlayerOneSteamNumber(int playerOneSteamNumber) {
+        this.playerOneSteamNumber = playerOneSteamNumber;
+    }
+
+    /**
+     * Gets player two steam number.
+     *
+     * @return the player two steam number
+     */
+    public int getPlayerTwoSteamNumber() {
+        return playerTwoSteamNumber;
+    }
+
+    /**
+     * Sets player two steam number.
+     *
+     * @param playerTwoSteamNumber the player two steam number
+     */
+    public void setPlayerTwoSteamNumber(int playerTwoSteamNumber) {
+        this.playerTwoSteamNumber = playerTwoSteamNumber;
+    }
+
+    /**
+     * Gets statistic type.
+     *
+     * @return the statistic type
+     */
+    public String getStatisticType() {
+        return statisticType;
+    }
+
+    /**
+     * Sets statistic type.
+     *
+     * @param statisticType the statistic type
+     */
+    public void setStatisticType(String statisticType) {
+        this.statisticType = statisticType;
+    }
+
+
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", date='" + date + '\'' +
+                ", name=" + name +
+                ", steamNumberOne=" + playerOneSteamNumber +
+                ", steamNumberTwo=" + playerTwoSteamNumber +
+                ", statisticType=" + statisticType +
+                '}';
+    }
+}
---
+++ src/main/java/counterStrikeManager/entity/User.java
@@ -0,0 +1,161 @@
+package counterStrikeManager.entity;
+
+import counterStrikeManager.entity.Report;
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ *
+ * @author ehollis98
+ *
+ */
+@Entity(name = "User")
+@Table(name ="user")
+public class User {
+
+    /**
+     * Instantiates a new User.
+     */
+    public User() {
+    }
+
+    /**
+     * Instantiates a new User.
+     *
+     * @param firstName    first name
+     * @param lastName     last name
+     * @param username     username
+     * @param password     password
+     */
+    public User(String firstName, String lastName, String username, String password) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.username = username;
+        this.password = password;
+    }
+
+    @Id
+    @Column(name = "user_Id")
+    @GeneratedValue(strategy = GenerationType.AUTO, generator = "native")
+    @GenericGenerator(name = "native",strategy = "native")
+    private int id;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "user_name")
+    private String username;
+
+    @Column(name = "pass")
+    private String password;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.EAGER)
+    private Set<Report> report = new HashSet<>();
+
+    /**
+     * Gets id.
+     *
+     * @return the id
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * Sets id.
+     *
+     * @param id the id
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    /**
+     * Gets first name.
+     *
+     * @return the first name
+     */
+    public String getFirstName() {
+        return firstName;
+    }
+
+    /**
+     * Sets first name.
+     *
+     * @param firstName the first name
+     */
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    /**
+     * Gets last name.
+     *
+     * @return the last name
+     */
+    public String getLastName() {
+        return lastName;
+    }
+
+    /**
+     * Sets last name.
+     *
+     * @param lastName the last name
+     */
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    /**
+     * Gets username.
+     *
+     * @return the username
+     */
+    public String getUsername() {
+        return username;
+    }
+
+    /**
+     * Sets username.
+     *
+     * @param username the username
+     */
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    /**
+     * Gets password.
+     *
+     * @return the password
+     */
+    public String getPassword() {
+        return password;
+    }
+
+    /**
+     * Sets password.
+     *
+     * @param password the password
+     */
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", username='" + username + '\'' +
+                ", userPassword='" + password + '\'' +
+                '}';
+    }
+}
