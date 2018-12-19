c783c6c4d77de0f1c17af765bdea3e6c6bbdbdee
Eric Hollis <ehollis@madisoncollege.edu>
2018-12-18 20:38:16 -0600

    Momentarily changing cleandb.sql for the time being.

--- src/test/resources/cleandb.sql
+++ src/test/resources/cleandb.sql
@@ -3,4 +3,4 @@ DELETE FROM role;
 DELETE FROM user;
 INSERT INTO user (user_Id, first_name, last_name, user_name, pass) VALUES (1, 'Eric', 'Hollis', 'ehollis98', 'root');
 INSERT INTO role (id, role_name, user_name, user_id) VALUES (1, 'admin', 'ehollis98', 1);
-INSERT INTO report (id, date, name, steam_number_one, steam_number_two, statistic_type) VALUES (1, 'TestReport', 0, 0, 'test');
+INSERT INTO report (id, name, steam_number_one, steam_number_two, statistic_type) VALUES (1, 'TestReport', 0, 0, 'test');
